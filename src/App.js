b2df6cea30cdb16d51e1f2d5180211513c1ff051
pomelobutter <pomelo0628@163.com>
2019-04-11 21:48:51 +0800

    理解state状态的使用

--- src/App.js
+++ src/App.js
@@ -4,6 +4,26 @@ import './App.css';
 import  Person  from './Person/Person';
 
 class App extends Component {
+state={
+  Persons: [
+    { name:"QQ",count:123},
+    { name:"wechat",count:213},
+    { name:"sina",count:321},
+  ]
+}
+
+switchNameHander=()=>{
+  // alert(123);
+
+  this.setState({
+    Persons: [
+      { name:"QQ1",count:1231},
+      { name:"wechat1",count:2131},
+      { name:"sina1",count:3211},
+    ]
+  })
+}
+
   render() {
     return (
       <div className="App">
@@ -20,9 +40,10 @@ class App extends Component {
           >
             Learn React
           </a>
-         <Person name="Pomelobutter" count="30"/>
-         <Person name="Yuuko" count="20"/>
-         <Person name="Jesse" count="10">大家好，一起来学习React吧</Person>
+          <button onClick={this.switchNameHander}>更改状态</button>
+         <Person name={this.state.Persons[0].name} count={this.state.Persons[0].count}/>
+         <Person name={this.state.Persons[1].name} count={this.state.Persons[1].count}/>
+         <Person name={this.state.Persons[2].name} count={this.state.Persons[2].count}>大家好，一起来学习React吧</Person>
         </header>
     
       </div>
